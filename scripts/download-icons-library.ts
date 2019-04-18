61e1b9ab8273b4d47fe86064cc05ce77f73ed994
Rafael Pérez <rafael.perez@imperdiblesoft.com>
2019-04-18 14:05:46 +0200

    Icons repo can be setup through .env file

--- scripts/download-icons-library.ts
+++ scripts/download-icons-library.ts
@@ -1,3 +1,44 @@
-import dotEnv from 'dotenv';
+import { config } from 'dotenv';
+import { Clone, CloneOptions, Cred } from 'nodegit';
+import { homedir } from 'os';
+import { join } from 'path';
+import rimraf from 'rimraf';
 
-dotEnv.config();
+import { error, info } from '../src/common/utils/logger';
+
+config();
+
+const LOCAL_PATH = join(__dirname, '../src/images/icons/');
+const { REPO_PASSPHRASE, REPO_PRIVATE_PATH, REPO_PUBLIC_PATH, REPO_URL } = process.env;
+
+if (
+  typeof REPO_PASSPHRASE !== 'undefined' &&
+  typeof REPO_PRIVATE_PATH !== 'undefined' &&
+  typeof REPO_PUBLIC_PATH !== 'undefined' &&
+  typeof REPO_URL !== 'undefined'
+) {
+  rimraf.sync(LOCAL_PATH);
+
+  const PUBLIC_KEY_PATH = join(homedir(), REPO_PUBLIC_PATH);
+  const PRIVATE_KEY_PATH = join(homedir(), REPO_PRIVATE_PATH);
+
+  const getSshKey = (userName: string) => Cred.sshKeyNew(userName, PUBLIC_KEY_PATH, PRIVATE_KEY_PATH, REPO_PASSPHRASE);
+
+  const cloneOptions: CloneOptions = {
+    fetchOpts: {
+      callbacks: {
+        credentials: (url: string, userName: string) => getSshKey(userName),
+      },
+    },
+  };
+
+  Clone.clone(REPO_URL, LOCAL_PATH, cloneOptions)
+    .then(() => {
+      info('Repository has been downloaded');
+    })
+    .catch((err: any) => {
+      error('Got an error trying to access the repo', err);
+    });
+} else {
+  error('There is some value missing in your Enviroment values. Please, check the Readme.md');
+}
